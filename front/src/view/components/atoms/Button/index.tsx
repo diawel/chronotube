ded2fd2091801828d9402203cdd3fb7036e700a7
diawel <masayazzrs+diawel@gmail.com>
2023-02-08 20:33:10 +0900

    [add] Footerを作成

---
+++ front/src/view/components/atoms/Button/index.tsx
@@ -0,0 +1,24 @@
+import { ReactNode } from 'react'
+import styled from 'styled-components'
+
+export type ButtonPropsType = {
+  handleClick: () => void
+  children: ReactNode
+}
+
+const Button: React.FC<ButtonPropsType> = (props) => {
+  const { handleClick, children } = props
+  const onClick = (): void => {
+    handleClick()
+  }
+  return <StyledButton onClick={onClick}>{children}</StyledButton>
+}
+
+const StyledButton = styled.button`
+  display: contents;
+  color: inherit;
+  background: none;
+  border: none;
+`
+
+export default Button
---
+++ front/src/view/components/molecules/Footer/index.tsx
@@ -0,0 +1,50 @@
+import { useContext } from 'react'
+import { color } from 'src/styles/color'
+import Text from '../../atoms/Text'
+import styled from 'styled-components'
+import { DeviceTypeContext } from 'src/index'
+import Wrap from '../../atoms/Wrap'
+import { Link } from 'react-router-dom'
+
+const Footer: React.FC = () => {
+  const deviceType = useContext(DeviceTypeContext)
+  switch (deviceType) {
+    case 'mobile':
+      return (
+        <StyledFooter justify="space-between">{FooterContent}</StyledFooter>
+      )
+    case 'desktop':
+      return <StyledFooter justify="flex-end">{FooterContent}</StyledFooter>
+  }
+}
+
+const textStyle = { color: color.darkGray, size: '12px' }
+
+const TextWrapper = styled.div`
+  margin: 8px 8px;
+`
+
+const FooterContent = (
+  <Wrap>
+    <a href="https://diawel.me/">
+      <TextWrapper>
+        <Text style={textStyle}>diawel.me</Text>
+      </TextWrapper>
+    </a>
+    <Link to="/privacy">
+      <TextWrapper>
+        <Text style={textStyle}>プライバシーポリシー</Text>
+      </TextWrapper>
+    </Link>
+  </Wrap>
+)
+
+const StyledFooter = styled.footer<{ justify: string }>(
+  (style) => `
+    margin: 0 8px;
+    display: flex;
+    justify-content: ${style.justify};
+  `
+)
+
+export default Footer
