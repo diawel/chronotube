cbe51508e2f07613519a28da4b9d6d0291fde3f5
diawel <masayazzrs+diawel@gmail.com>
2023-03-05 23:05:32 +0900

    [add] pages-Homeに再生履歴周りの情報を追加

--- src/view/pages/Home/Default/Abstract/index.tsx
+++ src/view/pages/Home/Default/Abstract/index.tsx
@@ -7,6 +7,7 @@ import { subscription } from 'src/common/utils/db/subscription'
 import SideSlider from 'src/view/components/atoms/SideSlider'
 import Wrap from 'src/view/components/atoms/Wrap'
 import { dateToString } from 'src/common/utils/dateToString'
+import { watchHistory } from 'src/common/utils/db/watchHistory'
 
 const Abstract: React.FC = () => {
   const channelCount = useLiveQuery(
@@ -15,6 +16,12 @@ const Abstract: React.FC = () => {
   const channelsFetched: Date | undefined = useLiveQuery(
     async () => await subscription.meta.get('fetched')
   )?.value
+  const historyCount = useLiveQuery(
+    async () => await watchHistory.histories.count()
+  )?.toString()
+  const latestPlaybackDate = useLiveQuery(
+    async () => await watchHistory.histories.toCollection().last()
+  )?.playbackDate
 
   const Blocks = [
     <Wrap key={0}>
@@ -22,14 +29,20 @@ const Abstract: React.FC = () => {
         title="登録チャンネル数"
         value={channelCount ? channelCount.toString() : ''}
       />
-      <TitledValue title="総再生数" value="なし" />
+      <TitledValue
+        title="総再生数"
+        value={historyCount ? historyCount.toString() : ''}
+      />
     </Wrap>,
     <Wrap key={1}>
       <TitledValue
         title="登録チャンネル最終更新"
         value={channelsFetched ? dateToString(channelsFetched) : ''}
       />
-      <TitledValue title="最新の再生履歴" value="なし" />
+      <TitledValue
+        title="最新の再生履歴"
+        value={latestPlaybackDate ? dateToString(latestPlaybackDate) : ''}
+      />
     </Wrap>,
   ]
 
