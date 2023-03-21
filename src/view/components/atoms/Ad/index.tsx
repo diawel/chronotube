9e0dad5e3a49e6ae215fa8e0e794b46cabb57e6a
diawel <masayazzrs+diawel@gmail.com>
2023-03-22 00:37:22 +0900

    [fix] deviceType変更時に広告のレイアウトが変更されるように修正

--- src/view/components/atoms/Ad/index.tsx
+++ src/view/components/atoms/Ad/index.tsx
@@ -1,22 +1,38 @@
-import { useEffect } from 'react'
+import { useContext, useEffect, useState } from 'react'
+import { DeviceContext } from 'src/index'
 
 const Ad: React.FC = () => {
-  useEffect(() => {
-    let adsbygoogle = (window as any).adsbygoogle
-    adsbygoogle = adsbygoogle || []
-    adsbygoogle.push({})
-  }, [])
+  const [isVisible, setIsVisible] = useState(true)
+  const deviceType = useContext(DeviceContext)
 
-  return (
-    <ins
-      className="adsbygoogle"
-      style={{ display: 'block' }}
-      data-ad-client="ca-pub-1491068566472383"
-      data-ad-slot="9540915657"
-      data-ad-format="auto"
-      data-full-width-responsive="true"
-    />
-  )
+  useEffect(() => {
+    if (isVisible) {
+      let adsbygoogle = (window as any).adsbygoogle
+      adsbygoogle = adsbygoogle || []
+      adsbygoogle.push({})
+    }
+  }, [isVisible])
+
+  useEffect(() => {
+    if (!isVisible) setIsVisible(true)
+  }, [isVisible])
+
+  useEffect(() => {
+    setIsVisible(false)
+  }, [deviceType])
+
+  if (isVisible)
+    return (
+      <ins
+        className="adsbygoogle"
+        style={{ display: 'block', textAlign: 'center' }}
+        data-ad-layout="in-article"
+        data-ad-format="fluid"
+        data-ad-client="ca-pub-1491068566472383"
+        data-ad-slot="4777250544"
+      />
+    )
+  return <></>
 }
 
 export default Ad
