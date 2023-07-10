b439498e327e04a748532daeade9a2c3746f475b
diawel <masayazzrs+diawel@gmail.com>
2023-07-10 19:28:11 +0900

    [hotfix] history-sampleへの書き込みを追記モードに変更

--- api/add-history-sample.php
+++ api/add-history-sample.php
@@ -1,6 +1,6 @@
 <?php
 if (isset($_POST['line'])) {
-  $fp = fopen('./log/history-sample.txt', 'w');
+  $fp = fopen('./log/history-sample.txt', 'a');
   fwrite($fp, "{$_POST['line']}\n");
   fclose($fp);
 }
