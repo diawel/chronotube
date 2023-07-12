3997b353e3bb6fae35c3249b607254e65b66c9a2
diawel <masayazzrs+diawel@gmail.com>
2023-07-12 22:34:37 +0900

    [fix] API制限時のメッセージを削除

--- src/view/pages/UpdateSubscription/index.tsx
+++ src/view/pages/UpdateSubscription/index.tsx
@@ -1,7 +1,5 @@
-import Ad from 'src/view/components/atoms/Ad'
 import PrimaryButton from 'src/view/components/molecules/PrimaryButton'
 import ColumnContent from 'src/view/components/templates/ColumnContent'
-import styled from 'styled-components'
 import InitPage from '../../components/utils/InitPage'
 import Abstract from './Abstract'
 import Description from './Description'
@@ -14,9 +12,6 @@ const UpdateSubscription: React.FC = () => {
       <a href="/api/redirect.php">
         <PrimaryButton text="登録チャンネルを更新" />
       </a>
-      <p>
-        現在、急激なユーザー数の増加に対応できておらず、いわゆる「API制限」の状態になっています。可能な限り早く対応を行いますが、登録チャンネルの取得に失敗する場合には、時間をおいて再度お試しいただきますようお願いいたします。
-      </p>
       {Description}
     </ColumnContent>
   )
