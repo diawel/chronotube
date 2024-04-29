95f1059caddaef5d09a981b714c4b71cf6c371ad
Diawel <38136957+diawel@users.noreply.github.com>
2024-04-29 22:42:46 +0900

    [init] package, eslintを整理

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,61 +1,18 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-    node: true,
-  },
-  parser: '@typescript-eslint/parser',
+  root: true,
+  env: { browser: true, es2020: true },
   extends: [
-    'plugin:react/recommended',
-    'standard-with-typescript',
-    'prettier', // 追加。他の設定の上書きを行うために、必ず最後に配置する。
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
   ],
-  overrides: [],
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-    project: './tsconfig.json',
-  },
-  plugins: ['react', '@typescript-eslint', 'import', 'import'],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react-refresh'],
   rules: {
-    'react/react-in-jsx-scope': 'off',
-    'react/prop-types': 'off',
-    'react/jsx-filename-extension': [
-      'error',
-      {
-        extensions: ['.jsx', '.tsx'],
-      },
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
     ],
-    'import/extensions': [
-      'error',
-      'ignorePackages',
-      {
-        js: 'never',
-        jsx: 'never',
-        ts: 'never',
-        tsx: 'never',
-      },
-    ],
-    '@typescript-eslint/consistent-type-definitions': 'off',
-    '@typescript-eslint/strict-boolean-expressions': 'off',
-    '@typescript-eslint/restrict-template-expressions': 'off',
   },
-  settings: {
-    'import/resolver': {
-      node: {
-        extensions: ['.js', '.jsx', '.ts', '.tsx'],
-      },
-      typescript: [
-        {
-          project: './',
-        },
-      ],
-    },
-  },
-  ignorePatterns: [
-    '.eslintrc.*',
-    'vite.config.*',
-    'vite-env.d.ts',
-    '*.test.tsx',
-  ],
 }
